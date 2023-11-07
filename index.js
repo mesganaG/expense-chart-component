fa1e227d4cf2fe7435609d848c039ffa520505e3
Mesgana Geletu <121146669+mesganaG@users.noreply.github.com>
2023-11-07 20:13:03 +0000

    90% compeleted

--- index.js
+++ index.js
@@ -1,61 +1,115 @@
 // Description: This file is used to create a chart using chart.js library
-
 fetch("./data.json")
-.then(response => response.json()).then(data => {
-    const days = data.map(day => day.day);
-    const amount = data.map(day => day.amount);
-    const ctx = document.getElementById('myChart');
+    .then(response => response.json()).then(data => {
+        // Get the data from the json file
+        const days = data.map(day => day.day);
+        const amount = data.map(day => day.amount);
+        const ctx = document.getElementById('myChart');
+        const maxAmount = Math.max(...amount);
 
-    new Chart(ctx, {
-       
-        type: 'bar',
-        data: {
-            labels: days,
-            datasets: [{
-                data: amount,
-                backgroundColor: 'hsl(10, 79%, 65%)',
-                borderColor: 'hsl(10, 79%, 65%)',
-                borderRadius: 5,
-                borderWidth: 2,
-            }]
-        },
-        option: {
-            scales: {
-                x: {
-                    grid: {
-                        beginAtZero: true,
-                        display: false, // Hide x-axis gridlines
-                        tickColor: 'blue'
+        // Get the total expense and my balance
+        const expenseEl = document.getElementById('total-expense');
+        const myBalanceEl = document.getElementById('my-balance');
+        const startingBalance = 921.48;
+        const totalExpense = amount.reduce((total, curr) => total + curr);
+        myBalanceEl.innerText = `$${startingBalance - totalExpense}`;
+        expenseEl.innerText = `$${totalExpense}`;
+
+        // Custom background color for the bar chart
+        const customBackgroundColor = amount.map(value => {
+            return value === maxAmount ? 'hsl(186, 34%, 60%)' : 'hsl(10, 79%, 65%)';
+        });
+        const hoverEffect = amount.map(value => {
+            return value === maxAmount ? 'hsl(186, 34%, 60%,0.7)' : 'hsl(10, 79%, 65%,0.7)';
+        });
+
+        let x = window.matchMedia("(max-width: 799px)");
+
+        let updateAspectRatio = (x) => {
+            if (x.matches) { // If media query matches
+                return 1.2;
+            } else {
+                return true;
+            }
+        };
+
+        const updatedRatio = updateAspectRatio(x);
+
+        // Create a chart using chart.js library
+        new Chart(ctx, {
+
+            type: 'bar',
+            data: {
+                labels: days,
+                datasets: [{
+                    data: amount,
+                    backgroundColor: customBackgroundColor,
+                    borderRadius: 5,
+                    borderWidth: 0,
+                    hoverBackgroundColor: hoverEffect,
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                aspectRatio: updatedRatio,
+                plugins: {
+                    legend: {
+                        display: false,
+                    },
+                    tooltip: {
+                        callbacks: {
+                            title: function (context) {
+                                return '';
+                            },
+                            label: function (context) {
+                                return '';
+                            },
+                            beforeLabel: function (context) {
+                                return '$' + context.dataset.data[context.dataIndex];
+                            },
+                        },
+                        enabled: true,
+                        yAlign: 'bottom',
+                        caretSize: 0,
+                        caretPadding: 7,
+                        displayColors: false,
+                        titleColor: 'hsl(0, 0%, 100%)',
+                        padding: 8,
+                    
+
                     }
                 },
-                y: {
-                    grid: {
-                        display: false, // Hide y-axis gridlines
-                        tickColor: 'green'
+                scales: {
+                    x: {
+                        beginAtZero: true,
+                        grid: {
+                            display: false, // Hide x-axis gridlines
+                        }
+                    },
+                    y: {
+                        beginAtZero: true,
+                        display: false,
                     }
+                },
+                layout: {
+                    padding: {
+                        left: 30,
+                        right: 30,
+                        bottom: 40,
+                        top: 10
+                    }
+
+
                 }
-            },
-            legend: {
-                display: true,
-                position: 'right',
-                labels: {
-                    fontColor: '#000'
-                }
-            },
-            layout: {
-                padding: {
-                    left: 50,
-                    right: 0,
-                    bottom: 0,
-                    top: 0
-                }
+
+
             }
-           
-        }
+
+        });
 
     });
 
-});
 
 //chartMain.defaults.scale.gridLines.drawOnChartArea = false;
 //const ctx = document.getElementById('myChart');
